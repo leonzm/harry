d74fbfcb146b698e242d200558ebb8a16df08dfe
zhaoman <zhaoman@daihoubang.com>
2017-05-12 17:42:07 +0800

    add comiler

---
+++ src/main/java/com/github/harry/core/compiler/Compiler.java
@@ -0,0 +1,13 @@
+package com.github.harry.core.compiler;
+
+/**
+ * @Author: Leon
+ * @CreateDate: 2017/5/12
+ * @Description:
+ * @Version: 1.0.0
+ */
+public interface Compiler {
+
+    Class<?> compile(String code);
+
+}
