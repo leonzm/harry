d7e91ea5c688c360162bd957b8e91036f6e0d313
zhaoman <zhaoman@daihoubang.com>
2017-05-16 16:56:42 +0800

    add groovy engine

---
+++ src/main/java/com/github/harry/core/groovy/GroovyException.java
@@ -0,0 +1,27 @@
+package com.github.harry.core.groovy;
+
+/**
+ * @Author: Leon
+ * @CreateDate: 2017/5/16
+ * @Description:
+ * @Version: 1.0.0
+ */
+public class GroovyException extends Exception{
+
+    public GroovyException() {
+        super();
+    }
+
+    public GroovyException(String message) {
+        super(message);
+    }
+
+    public GroovyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GroovyException(Throwable cause) {
+        super(cause);
+    }
+
+}
